353b662ce52208db7c064c4f60cae9b28f8cf1bc
袁嘉宝 <331954685@qq.com>
2026-09-26 19:49:58 +0800

    .

--- index/index03.js
+++ index/index03.js
@@ -1,7 +1,7 @@
 location.href = 'https://19557399143.github.io/index/index03.html#indexx'
 const h = 'https://19557399143.github.io/index/index03.html#indexx'
 const indexx = document.querySelector('#indexx')
-const img = document.querySelector('#im')
+const img = indexx.querySelector('#img')
 // while (true) {
     // location.href !== h?indexx.style.display = 'none':h
 // }
