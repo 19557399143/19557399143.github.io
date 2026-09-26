e8860c80d2fa7f45a66b8b2293059ac7cf14c4c9
袁嘉宝 <331954685@qq.com>
2026-09-26 20:50:47 +0800

    .

--- index/index03.js
+++ index/index03.js
@@ -1,8 +1,22 @@
-location.href = 'https://19557399143.github.io/index/index03.html#indexx'
-const h = 'https://19557399143.github.io/index/index03.html#indexx'
-const indexx = document.querySelector('#indexx')
-const img = indexx.querySelector('#img')
-// while (true) {
-    // location.href !== h?indexx.style.display = 'none':h
-// }
-indexx.addEventListener()
+location.search = 'indexx'
+const hf1 = 'https://19557399143.github.io/index/index03.html?indexx'
+const indexx = qs('#indexx')
+const aix = qs('#aix')
+const img = qs('#img')
+function qs(n) {
+    document.querySelector(n)
+}
+function op1() {
+    location.search = 'indexx-in'
+}
+function op2() {
+    location.search = 'aix-in'
+}
+while (true) {
+    if (location.href !== h) {
+        indexx.style.display = 'none'
+        indexx.style.display = 'none'
+    }
+}
+indexx.addEventListener('dblclick',op1)
+aix.addEventListener('dblclick',op2)
